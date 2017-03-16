f6e551f5dcef2ad2083bdbefb8dd494d1a8376a9
spaddex <spaddex@swehack.org>
2017-03-16 16:55:39 +0100

    Clean up a little
    Removed typos, removed an external lib. Still ugly as fu*k though.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Internet-Tester
 
-While backpacking through Asia, I noticed the internet could suddenly disapear. Sometimes it would come back 30 seconds later, sometimes 2 hours later. To avoid having to baby-sit the computer, I wrote this script. 
+A simple and ugly script which tries to access google every 10 seconds until a successful request can be made.   
+When a successful request is made, it will beep, and then start a speedtest.    
+`chmod +x internet.py speedtest-cli`   
+To run, simple: `./internet.py`   
 
-The idea is that you start the script (./internet.py) when you loose the internet, and it it will try to connect to the internet every 10 seconds. When it's connected, it does a few beeping sound and tests the internet connection.
-
-Requires colorama (pip3 install colorama)
+Dependencies - "requests":  
+`sudo apt install python3-pip -y && pip3 install requests`
--- internet.py
+++ internet.py
@@ -4,54 +4,67 @@
 """-------------IMPORTS------------"""
 import time
 import requests
-import colorama
 import subprocess
 import signal
 import sys
+
 """--------------------------------"""
 
-forsok=0
-def anslutning():
-	global forsok
-	try:
-		global svar
-		svar=requests.get("http://www.google.com")
-		
-	except:
-		if forsok < 1:
-			print ('\033[31m'+"[+]Nope, No internet, trying again in 10 seconds. \n ")
-			time.sleep(10)
-			forsok+=1
-			anslutning()
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-		else:
-			print('\033[31m'+"[+]Still no internet, vill retry again in 10 seconds")
-			if (forsok%6)==0:
-				print('[+]VWe haven\'t had internet in ' +str(forsok/6)+ " minutes =/ \n")
-			time.sleep(10)
-			forsok+=1
-			anslutning()
-	
-	if svar.status_code == 200:
-		print('\033[32m'+"[+]Wo-ho!")
-		success()
-			
+forsok = 0
+
+
+def anslutning():
+    global forsok
+    try:
+        global svar
+        svar = requests.get("http://www.google.com")
+
+    except:
+        if forsok < 1:
+            print(bcolors.FAIL + "[+] Nope, no internet, trying again in 10 seconds. \n ")
+            time.sleep(10)
+            forsok += 1
+            anslutning()
+
+        else:
+            print(bcolors.FAIL + "[+] Still no internet. Will retry again in 10 seconds")
+            if (forsok % 6) == 0:
+                print('[+] We haven\'t had internet in ' + str(forsok / 6) + " minutes =/ \n")
+            time.sleep(10)
+            forsok += 1
+            anslutning()
+
+    if svar.status_code == 200:
+        print(bcolors.OKGREEN + "[+] Wo-ho!")
+        success()
+
+
 def success():
-	for a in range(0,5):
-		print("\a We got internet!!!! :D")
-		time.sleep(1)
-		
-	print("[+]Testing the connection, uno momento!")
-	print("\033[34m")
-	subprocess.Popen([r"./speedtest-cli"])
-	time.sleep(150)
-		
+    for a in range(0, 5):
+        print( bcolors.OKGREEN + "\a We got internet!!!! :D")
+        time.sleep(1)
+
+    print( bcolors.OKGREEN + "[+] Testing the connection, uno momento!")
+    print("\033[34m")
+    subprocess.Popen([r"./speedtest-cli"])
+    time.sleep(150)
+
+
 def signal_handler(signal, frame):
-    print ('\n [+]And we\re done!')
-    sys.exit(0)	
-    
-signal.signal(signal.SIGINT, signal_handler) #Sätter en signal handler som gör att ctrl+c ej skickar felmeddelande
+    print(bcolors.OKBLUE + "\n [+] And we're done!")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)  # Sätter en signal handler som gör att ctrl+c ej skickar felmeddelande
 
 anslutning()
-
-
